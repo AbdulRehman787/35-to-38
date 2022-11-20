e7ac065122ba95499442288abbae2a28d878ba6d
AbdulRehman787 <88674059+AbdulRehman787@users.noreply.github.com>
2022-11-20 13:57:08 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+// function abc(){
+//     var fname="Abdul";
+//     var lname="Rehman"
+//     console.log(fname + " " + lname)
+// }
+
+// abc()
+// abc()
+
+// function abc(){
+//     var date = new Date();
+//     var today=date.getDay();
+//     var todate=date.getDate();
+//     var final = today + todate;
+//     console.log(final)
+// }
+// abc()
+
+// function abc(){
+//     var first= prompt("Enter your first Name");
+//     var last = prompt("Enter your last Name");
+//     var final =  first+ " " + last;
+//     console.log(final)
+// }
+// abc()
+
+// function abc(){
+//     var first = +prompt("Enter a first number");
+//     var second = +prompt("Enter a second number");
+//     console.log(`Your first number is ${first} your second number is ${second}`)
+// }
+// abc()
